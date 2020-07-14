8c272377681ae8a31b5ca65cea5d17be794fac4c
hyecheon <rainbow880616@gmail.com>
2020-07-14 15:42:25 +0900

    Initial commit

---
+++ microservices/review-service/src/main/kotlin/com/hyecheon/reviewservice/ReviewServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.hyecheon.productcompositeservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductCompositeServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductCompositeServiceApplication>(*args)
+}
